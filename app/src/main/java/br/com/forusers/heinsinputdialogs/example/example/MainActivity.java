865404e92c2c9020c63f3fb5ded92d2bbf4da260
Maicon Hellmann <maiconhellmann@gmail.com>
2016-12-05 08:38:08 -0200

    Bintray configuration

---
+++ app/src/main/java/br/com/forusers/heinsinputdialogs/example/example/MainActivity.java
@@ -0,0 +1,15 @@
+package br.com.forusers.heinsinputdialogs.example.example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.forusers.heinsinputdialogs.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
